05145fdf852e3dd85fa12518b4b36f105f2f943d
Буланов Александр <alexbulanovgoodmail@gmail.com>
2024-12-15 18:12:18 +0300

    Настройка динамических лейаутов

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -1,10 +1,29 @@
-<script setup lang="ts">
-import AppHeader from "./AppHeader.vue";
+<script setup>
+import { shallowRef, watch } from "vue";
+import { useRoute } from "vue-router";
+import AppLayoutDefault from "@/layouts/DefaultLayout.vue";
+
+const route = useRoute();
+const layout = shallowRef(null);
+
+// Наблюдаем за изменением маршрута
+watch(
+  () => route.meta,
+  async (meta) => {
+    try {
+      // Пробуем найти компонент из свойства meta и динамически импортировать его
+      const component = await import(`./${meta.layout}.vue`);
+      layout.value = component?.default || AppLayoutDefault;
+    } catch (e) {
+      // Если компонент не найдет, добавляем шаблон по-умолчанию
+      layout.value = AppLayoutDefault;
+    }
+  }
+);
 </script>
 
 <template>
-  <div class="app_layout">
-    <AppHeader />
+  <component :is="layout">
     <slot />
-  </div>
+  </component>
 </template>
---
+++ frontend/src/layouts/DefaultLayout.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import DefaultHeader from "./DefaultHeader.vue";
+</script>
+
+<template>
+  <div class="app_layout">
+    <DefaultHeader />
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.default-layout {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+}
+</style>
