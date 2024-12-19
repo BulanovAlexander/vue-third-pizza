17533741077b362eefd8a467c097a6ff4dba2dc4
Буланов Александр <alexbulanovgoodmail@gmail.com>
2024-12-19 20:08:17 +0300

    Подключение хранилищ к компонентам

---
+++ frontend/src/views/SuccessView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import AppPopup from "@/common/components/popup/AppPopup.vue";
+import AppPopupTitle from "@/common/components/popup/AppPopupTitle.vue";
+import AppPopupButton from "@/common/components/popup/AppPopupButton.vue";
+</script>
+
+<template>
+  <AppPopup>
+    <RouterLink :to="{ name: 'orders' }" class="close">
+      <span class="visually-hidden">Закрыть поп-ап</span>
+    </RouterLink>
+    <AppPopupTitle> Спасибо за заказ </AppPopupTitle>
+    <p>Мы начали готовить Ваш заказ, скоро привезём его вам ;)</p>
+    <AppPopupButton>
+      <RouterLink :to="{ name: 'orders' }" class="button">
+        Отлично, я жду!
+      </RouterLink>
+    </AppPopupButton>
+  </AppPopup>
+</template>
